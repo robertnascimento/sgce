f5feda576bf87590d997e8a7e8b6bf2d9793e811
robertnascimento <robertiver13.03@gmail.com>
2022-11-28 22:46:41 -0300

    CRIAÇÃO DO CRUD TIPOS DE PRODUTOS

--- core/views.py
+++ core/views.py
@@ -24,7 +24,7 @@ def fornecedor_cadastro(request):
 
     return render(request,'fornecedorcad.html',contexto)
 
-def fornecedorEdit(request,id):
+def fornecedor_Edit(request,id):
     frnc = Fornecedor.objects.get(pk=id)
     
     form = FornecedorForm(request.POST or None,request.FILES or None, instance=frnc)
@@ -43,3 +43,49 @@ def fornecedor_remover(request, id):
     frnc.delete()
     return redirect('lista_fornecedores')
 
+
+
+
+
+
+"""
+TIPO PRODUTOS
+"""
+
+def tipoProduto_Lista(request):
+    tpProd = TipoProduto.objects.all()
+    contexto = {
+        'lista_tipoProduto': tpProd
+    }
+    return render(request,'tipoprodutolista.html',contexto)
+
+def tipoProduto_Cadastro(request):
+    form = TipoProdutoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('lista_tipoProduto')
+
+    contexto = {
+        'form' : form
+    }
+
+    return render(request,'tipoprodutocad.html',contexto)
+
+def tipoProduto_Edit(request,id):
+    tprd = TipoProduto.objects.get(pk=id)
+
+    form = TipoProdutoForm(request.POST or None,instance= tprd)
+    if form.is_valid():
+        form.save()
+        return redirect('lista_tipoProduto')
+
+    contexto = {
+        'form' : form
+    }
+
+    return render(request,'tipoprodutoedit.html',contexto)
+
+def tipoProduto_Remover(request,id):
+    tprd = TipoProduto.objects.get(pk=id)
+    tprd.delete()
+    return redirect('lista_tipoProduto')
--- sgce/urls.py
+++ sgce/urls.py
@@ -17,13 +17,20 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from core.views import fornecedor_lista,fornecedorEdit,fornecedor_cadastro,fornecedor_remover
+from core.views import fornecedor_lista,fornecedor_Edit,fornecedor_cadastro,fornecedor_remover
+from core.views import tipoProduto_Edit,tipoProduto_Cadastro,tipoProduto_Lista,tipoProduto_Remover
 
 urlpatterns = [
-    path('fornecedoresEdit/<int:id>',fornecedorEdit,name='edit_fornecedor'),
+    path('fornecedoresEdit/<int:id>',fornecedor_Edit,name='edit_fornecedor'),
     path('fornecedoresCad/',fornecedor_cadastro,name='cad_fornecedores'),
     path('fornecedoresList/',fornecedor_lista,name='lista_fornecedores'),
     path('fornecedoresDelete/<int:id>',fornecedor_remover,name='fornecedore_remover'),
+
+    path('tipoProdutoEdit/<int:id>',tipoProduto_Edit,name='edit_tipoProduto'),
+    path('tipoProdutoCad/',tipoProduto_Cadastro,name='cad_tipoProduto'),
+    path('tipoProdutoList/',tipoProduto_Lista,name='lista_tipoProduto'),
+    path('tipoProdutoDelete/<int:id>',tipoProduto_Remover,name='del_tipoProduto'),
+
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
